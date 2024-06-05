a7bfb715d551ae87ad69960039b549e846f306f0
trishamasbate <trisha.masbate@gmail.com>
2024-06-05 16:53:54 -0600

    Assigned the variables for classes in script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const cityInput = document.querySelector(".city-input");
+const searchButton = document.querySelector(".search-btn");
+const weatherDataDiv = document.querySelector(".weather-data");
+const currentWeatherDiv = document.querySelector(".current-weather");
+const weatherCardsDiv = document.querySelector(".weather-cards");
+
+const API_KEY = "d71fd30353f26675eaa4b5a0551932ef"; // API key for OpenWeatherMap API
